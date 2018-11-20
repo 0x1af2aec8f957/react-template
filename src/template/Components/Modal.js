650e53bfb8051651422b8b3483930066bd3571c4
M <603803799@qq.com>
2018-11-20 18:35:06 +0800

    Update Modal.js

--- src/template/Components/Modal.js
+++ src/template/Components/Modal.js
@@ -4,11 +4,15 @@ import classNames from 'classnames'
 
 export default class Modal extends Component {
 
-  constructor ({defaultShowState/*bool*/}) {
+  constructor ({defaultShow/*bool*/}) {
     super(arguments[0])
     this.state = {
-      showState: defaultShowState,
+      showState: defaultShow,
     }
+  }
+
+  componentWillReceiveProps ({show: showState}) {
+    if (showState !== this.props.show) this.setState({showState})
   }
 
   handleClose = (closeType = true) => {
@@ -19,42 +23,41 @@ export default class Modal extends Component {
   }
 
   render () {
-    const {className, mode/*card*/, title, onOk, OnCancel, ...otherProps} = this.props
+    const {className, defaultShow, show, mode/*card*/, title, onOk, OnCancel, ...otherProps} = this.props
     return (
-      this.state.showState ? (
-        <div className={classNames('modal', className)}>
-          <div className="modal-background" onClick={this.handleClose}/>
-          {
-            mode === 'card' ? (
-              <div className="modal-card">
-                <header className="modal-card-head">
-                  <p className="modal-card-title">{title}</p>
-                  <button className="delete"
-                          aria-label="close"
-                          onClick={this.handleClose}/>
-                </header>
-                <section className="modal-card-body" {...otherProps}/>
-                <footer className="modal-card-foot">
-                  <button className="button is-success"
-                          onClick={() => onOk() && this.handleClose(false)}>
-                    确定
-                  </button>
-                  <button className="button"
-                          onClick={() => OnCancel() && this.handleClose(false)}>
-                    取消
-                  </button>
-                </footer>
-              </div>
-            ) : (
-              <div className="modal-content" {...otherProps}/>
-            )
-          }
-          {mode !== 'card' && (
-            <button className="modal-close is-large" aria-label="close"
-                    onClick={this.handleClose}/>
-          )}
-        </div>
-      ) : null)
+      <div className={classNames('modal', className, this.state.showState && 'is-active')}>
+        <div className="modal-background" onClick={this.handleClose}/>
+        {
+          mode === 'card' ? (
+            <div className="modal-card">
+              <header className="modal-card-head">
+                <p className="modal-card-title">{title}</p>
+                <button className="delete"
+                        aria-label="close"
+                        onClick={this.handleClose}/>
+              </header>
+              <section className="modal-card-body" {...otherProps}/>
+              <footer className="modal-card-foot">
+                <button className="button is-success"
+                        onClick={() => onOk() && this.handleClose(false)}>
+                  确定
+                </button>
+                <button className="button"
+                        onClick={() => OnCancel() && this.handleClose(false)}>
+                  取消
+                </button>
+              </footer>
+            </div>
+          ) : (
+            <div className="modal-content" {...otherProps}/>
+          )
+        }
+        {mode !== 'card' && (
+          <button className="modal-close is-large" aria-label="close"
+                  onClick={this.handleClose}/>
+        )}
+      </div>
+    )
   }
 }
 
@@ -62,7 +65,8 @@ Modal.propTypes = {
   className: PropTypes.string,
   mode: PropTypes.oneOf(['card']),
   title: PropTypes.string,
-  defaultShowState: PropTypes.bool,
+  defaultShow: PropTypes.bool,
+  show: PropTypes.bool,
   onOk: PropTypes.func,
   onClose: PropTypes.func,
   OnCancel: PropTypes.func,
