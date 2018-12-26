b8c117a5d73f614624d3bc1eef5679a0acbbbb75
M <603803799@qq.com>
2018-12-26 16:52:57 +0800

    Update React.js

--- src/config/React.js
+++ src/config/React.js
@@ -14,6 +14,19 @@ const instance = axios.create({
   // timeout: 1000,
 })
 
+/* instance.interceptors.request.use(function (config) { // use formData
+  // Do something before request is sent
+  return {
+    ...config,
+    transformRequest: [function (data, headers) {
+      // Do whatever you want to transform the data
+      return Object.entries(data).reduce((acc, cur) => acc.append(cur[0], cur[1]) || acc, new FormData());
+    }]};
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+}) */
+
 instance.interceptors.response.use(function (/*response*/{data}) {
   // Do something with response data
   data.status === 401 && (window.location.href = '/login')
